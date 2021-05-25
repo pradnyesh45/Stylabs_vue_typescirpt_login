8d30c67d7068f23781e62fb31485d7c174d05174
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-05-25 17:39:15 +0530

    created vue app

---
+++ Backend/models/user.ts
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const Users = mongoose.mode("User", userSchema);
+
+module.exports = Users;
